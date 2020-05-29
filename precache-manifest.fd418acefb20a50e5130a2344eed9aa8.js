ee0b36e59f9c80514e3908984594dfe8bc6fc37b
AA <adi.2002.gwl@gmail.com>
2020-05-29 17:54:01 +0530

    Updates

---
+++ precache-manifest.fd418acefb20a50e5130a2344eed9aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0dc7570339971f85c80d34788ad3e9f",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "fd50290e27b8047cf837",
+    "url": "/todo/static/css/main.bd1372ff.chunk.css"
+  },
+  {
+    "revision": "5a0304b334159f020559",
+    "url": "/todo/static/js/2.45affbc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.45affbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd50290e27b8047cf837",
+    "url": "/todo/static/js/main.f868c21f.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
